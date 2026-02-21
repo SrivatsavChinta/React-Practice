4b75ff5b19213673e945e20421da879c67de216b
SrivatsavChinta <srivatsavchinta2002@gmail.com>
2026-02-21 14:17:39 +0530

    checkpoint: dom drag and drop

--- vanilla-js/drag-drop/index.js
+++ vanilla-js/drag-drop/index.js
@@ -1,21 +1,12 @@
 const dropItem = document.querySelector(".drop-item");
 const boxes = document.querySelectorAll(".box");
 
-let draggedItem = null;
-
-dropItem.addEventListener("dragstart", (e) => {
-  draggedItem = e.target;
-});
-
 boxes.forEach((box) => {
   box.addEventListener("dragover", (e) => {
     e.preventDefault();
   });
 
-  box.addEventListener("drop", (e) => {
-    e.preventDefault();
-    if (draggedItem) {
-      box.appendChild(draggedItem);
-    }
+  box.addEventListener("drop", () => {
+    box.appendChild(dropItem);
   });
 });
